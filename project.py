bb61400f90bb715d8412a3138c7a90f27aba17b4
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-01-18 20:47:18 +0530

    Add files via upload

---
+++ project.py
@@ -0,0 +1,48 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+
+direction = input("Which direction do you want to cross the road ? left or right")
+direct = direction.lower()
+
+if direct == "right":
+    print("You died in a car accident!  Game Over!!!...")
+if direct == "left":
+ choice1 = input("You have come to a lake. There is  a island in the middle do you want to wait for a boat or swim across ? Type Swim or Wait").lower()
+ if choice1 == "swim":
+    print("You were eaten by an anaconda.! Game Over!!!...")
+ if choice1 == "wait":
+   choice2 =  input("You have entered in a room with three gates. Red, Yellow and Blue which gate will you chose? Red or Yellow or Blue ").lower()
+   if choice2 == "red":
+      print("You jumped into lava you died. Game Over!!!...")
+   if choice2 == "blue":
+      print("You were eaten by Lions. Game over!!!...")
+   if choice2 == "yellow":
+      print("Woo Hoo You were able to find treasure!!!!")
+else: 
+   print("Enter a valid choice.....")
