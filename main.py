2c30716878bcbf1a6f42034ee95fe897df931036
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-03-06 00:09:43 +0530

    Add files via upload

--- main.py
+++ main.py
@@ -1,59 +1,24 @@
-class Library:
-    def __init__(self, listOfBooks):
-        self.books = listOfBooks
+import random
+randNumber = random.randint(1, 100)
+userGuess = None
+guesses = 0
 
-    def displayAvailableBooks(self):
-        print("Books present in this library are: ")
-        for book in self.books: 
-            print(" *" + book)
-    
-    def borrowBook(self, bookName):
-        if bookName in self.books:
-            print(f"You have been issued {bookName}. Please keep it safe and return it within 30 days")
-            self.books.remove(bookName)
-            return True
+while(userGuess != randNumber):
+    userGuess = int(input("Enter your guess: "))
+    guesses += 1
+    if(userGuess==randNumber):
+        print("You guessed it right!")
+    else:
+        if(userGuess>randNumber):
+            print("You guessed it wrong! Enter a smaller number")
         else:
-            print("Sorry, This book is either not available or has already been issued to someone else. Please wait until the book is available")
-            return False
+            print("You guessed it wrong! Enter a larger number")
 
-    def returnBook(self, bookName):
-        self.books.append(bookName)
-        print("Thanks for returning this book! Hope you enjoyed reading it. Have a great day ahead!")
+print(f"You guessed the number in {guesses} guesses")
+with open("hiscore.txt", "r") as f:
+    hiscore = int(f.read())
 
-class Student: 
-    def requestBook(self):
-        self.book = input("Enter the name of the book you want to borrow: ")
-        return self.book
-
-    def returnBook(self):
-        self.book = input("Enter the name of the book you want to return: ")
-        return self.book
-         
-
-if __name__ == "__main__":
-    centraLibrary = Library(["Algorithms", "Django", "Clrs", "Python Notes"])
-    student = Student()
-    # centraLibrary.displayAvailableBooks()
-    while(True):
-        welcomeMsg = '''\n ====== Welcome to Central Library ======
-        Please choose an option:
-        1. List all the books
-        2. Request a book
-        3. Add/Return a book
-        4. Exit the Library
-        '''
-        print(welcomeMsg)
-        a = int(input("Enter a choice: "))
-        if a == 1:
-            centraLibrary.displayAvailableBooks()
-        elif a == 2:
-            centraLibrary.borrowBook(student.requestBook())
-        elif a == 3:
-            centraLibrary.returnBook(student.returnBook())
-        elif a == 4:
-            print("Thanks for choosing Central Library. Have a great day ahead!")
-            exit()
-        else:
-            print("Invalid Choice!")
-
-        
+if(guesses<hiscore):
+    print("You have just broken the high score!")
+    with open("hiscore.txt", "w") as f:
+        f.write(str(guesses))
