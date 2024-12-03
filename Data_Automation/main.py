a897aeede83e33868476bd05178a6b6671ba1128
Rounak Mishra <133516852+rounakm535@users.noreply.github.com>
2024-12-03 11:10:07 +0530

    Add files via upload

---
+++ Data_Automation/main.py
@@ -0,0 +1,66 @@
+from bs4 import BeautifulSoup
+import requests
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+
+URL = "https://appbrewery.github.io/Zillow-Clone/"
+
+header = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) "
+                  "Chrome/84.0.4147.125 Safari/537.36",
+    "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+}
+
+response = requests.get(URL, headers=header)
+
+data = response.text
+soup = BeautifulSoup(data, "html.parser")
+
+# Links to all the elements
+all_link_elements = soup.select(".StyledPropertyCardDataWrapper a")
+all_links = [link["href"] for link in all_link_elements]
+print(f"There are {len(all_links)} links to individual listings in total: \n")
+print(all_links)
+
+# Addresses to all the elements
+all_address_elements = soup.select(".StyledPropertyCardDataWrapper address")
+all_addresses = [address.get_text().replace(" | ", " ").strip() for address in all_address_elements]
+print(f"\n After having been cleaned up, the {len(all_addresses)} addresses now look like this: \n")
+print(all_addresses)
+
+# Price of all the elements
+all_price_elements = soup.select(".PropertyCardWrapper span")
+all_prices = [price.get_text().replace("/mo", "").split("+")[0] for price in all_price_elements if "$" in price.text]
+print(f"\n After having been cleaned up, the {len(all_prices)} prices now look like this: \n")
+print(all_prices)
+
+#-------------------------------------------- SELENIUM USAGE ----------------------------------------------------
+
+
+chrome_options = webdriver.ChromeOptions()
+chrome_options.add_experimental_option("detach", True)
+driver = webdriver.Chrome(options=chrome_options)
+
+for n in range(len(all_links)):
+    driver.get("https://forms.gle/y8Tqbc6Lc7eNmC8w5")
+    time.sleep(2)
+
+    # Use the xpath to select the "short answer" fields in your Google Form.
+    # Note, your xpath might be different if you created a different form.
+    address = driver.find_element(by=By.XPATH,
+                                  value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[1]/div/div/div[2]/div/div['
+                                        '1]/div/div[1]/input')
+    price = driver.find_element(by=By.XPATH,
+                                value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[2]/div/div/div[2]/div/div[1]/div/div['
+                                      '1]/input')
+    link = driver.find_element(by=By.XPATH,
+                               value='//*[@id="mG61Hd"]/div[2]/div/div[2]/div[3]/div/div/div[2]/div/div[1]/div/div['
+                                     '1]/input')
+    submit_button = driver.find_element(by=By.XPATH,
+                                        value='//*[@id="mG61Hd"]/div[2]/div/div[3]/div[1]/div[1]/div')
+
+    address.send_keys(all_addresses[n])
+    price.send_keys(all_prices[n])
+    link.send_keys(all_links[n])
+    submit_button.click()
